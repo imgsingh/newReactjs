05899ae8952d83fea4e585014dcfd09cc31ebffa
gursimran.singh <gursimran.singh@timesinternet.in>
2022-11-04 16:49:14 +0530

    kitty example

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -4,16 +4,59 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+         src={props.user.avatarUrl}
+         alt={props.user.name} />
+  );
+}
 
-const element = <Welcome name="imgsingh" />;
-root.render(element);
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />
+);
 
 
 // If you want to start measuring performance in your app, pass a function
