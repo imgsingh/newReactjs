c2eb8ed8fd6bde93f139727a190fc1e116fa37a2
gursimran.singh <gursimran.singh@timesinternet.in>
2022-11-04 17:01:29 +0530

    time example

--- new-app/src/index.js
+++ new-app/src/index.js
@@ -4,59 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-         src={props.user.avatarUrl}
-         alt={props.user.name} />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
-    </div>
-  );
-}
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author} />
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
-  }
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author} />
-);
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+setInterval(function (){root.render(<Clock date={new Date()}/>);}, 1000);
 
 
 // If you want to start measuring performance in your app, pass a function
